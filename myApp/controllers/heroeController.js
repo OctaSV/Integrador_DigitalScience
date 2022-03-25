c9aa2fc77d90ed73be1757728c162b993315d41d
OctaSV <osonciniviola@udesa.edu.ar>
2022-03-25 17:46:31 -0300

    index y show

--- myApp/controllers/heroeController.js
+++ myApp/controllers/heroeController.js
@@ -3,6 +3,15 @@ const heroes = require('../data/science')
 const heroeController = {
     index: function(req, res) {
         return res.send(heroes.lista)
+    },
+    show: function (req, res) {
+        for (let i = 0; i < heroes.lista.length; i++) {
+           if (heroes.lista[i].id == req.params.id) {
+               res.send(`Hola, mi nombre es ${heroes.lista[i].nombre} y soy ${heroes.lista[i].profesion}`)
+           } else{
+               res.send('No encontramos al científico indicado. Por favor, elija otro id')
+           }
+        }
     }
 }
 
--- myApp/routes/heroes.js
+++ myApp/routes/heroes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const heroeController = require('../controllers/heroeController');
 
 router.get('/', heroeController.index);
+router.get('/detalle/:id?', heroeController.show)
 
 
 module.exports = router;
