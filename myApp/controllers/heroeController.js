3544b3cae89e1c570b589f64be665d0e450f6420
OctaSV <osonciniviola@udesa.edu.ar>
2022-03-29 14:47:34 -0300

    gitignore

--- myApp/controllers/heroeController.js
+++ myApp/controllers/heroeController.js
@@ -5,13 +5,19 @@ const heroeController = {
         return res.send(heroes.lista)
     },
     show: function (req, res) {
-        for (let i = 0; i < heroes.lista.length; i++) {
-           if (heroes.lista[i].id == req.params.id) {
-               res.send(`Hola, mi nombre es ${heroes.lista[i].nombre} y soy ${heroes.lista[i].profesion}`)
-           } else{
-               res.send('No encontramos al científico indicado. Por favor, elija otro id')
-           }
+        let heroe = heroes.lista[req.params.id -1];
+        if (heroe != undefined) {
+            res.send(`Hola, mi nombre es ${heroe.nombre} y soy ${heroe.profesion}`);
+        } else{
+            res.send('No encontramos al cientifico indicado. Por favor , elija otro id.')
         }
+    //     for (let i = 0; i < heroes.dataScience.lista.lenght ; i++) {
+    //        if (heroes.lista[i].id == req.params.id) {
+    //            res.send(`Hola, mi nombre es ${heroes.lista[i].nombre} y soy ${heroes.lista[i].profesion}`)
+    //        } else{
+    //            res.send('No encontramos al científico indicado. Por favor, elija otro id')
+    //        }
+    //     }
     }
 }
 
